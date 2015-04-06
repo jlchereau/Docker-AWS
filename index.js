70b432b230eec10a2640a221d92b50cea1a802f5
vtorreci <vtorreci@gmail.com>
2015-04-06 17:03:18 +0100

    New feature

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello world\n');
+    res.send('Hello world with a great new feature\n');
 });
 
 app.listen(PORT);
